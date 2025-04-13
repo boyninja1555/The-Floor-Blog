c05ccd33259adc21de6651e06ea4639a8d332e43
boyninja15 <flappyfloorg@gmail.com>
2025-04-13 15:38:21 -0500

    Fixed scripts to be in the right path(s)

--- scripts/editor.bat
+++ scripts/editor.bat
@@ -1,4 +1,6 @@
 @echo off
+cd ..
 jekyll serve
-start "http://localhost:4000/admin"
+start http://localhost:4000/admin
 pause
+cd scripts
--- scripts/publish.bat
+++ scripts/publish.bat
@@ -1,6 +1,8 @@
 @echo off
+cd ..
 git pull
 git add .
 git commit -m "New Article: %1"
 git push
 pause
+cd scripts
